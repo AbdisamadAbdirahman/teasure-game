bfef09b6f8f2b054ea178b1bb9cbf17e0d6bc421
AbdisamadAbdirahman <abdisamad.abdirahman@laetottenham.ac.uk>
2024-12-16 16:52:54 +0000

    first commit

--- classes.py
+++ classes.py
@@ -1,3 +1,12 @@
+import time
+class Weapon:
+    def __init__(self, name, damage, durability):
+        self.name = name
+        self.damage = damage
+        self.durability = durability
+    def use(self, monster: object):
+        monster.health -= self.damage
+        self.durability -= 1
 class Player:
     def __init__(self,name, colour, health):
         self.name = name
@@ -14,25 +23,64 @@ class Player:
             if dropper == 5:
                 print("You ignored the item and kept moving.")
             elif dropper <= 4 and dropper > -1:
+                temp = self.inventory[dropper]
                 self.inventory.pop(dropper)
-                print("You picked up the item and discarded the other item.")
+                print(f"You picked up the item and discarded {temp}.")
                 self.inventory.append(item)
             else:
                 print("Please enter a valid number")
-    def fight(self, monster: object):
-        print(f"You killed the monster {monster.name}")
-    
-    def interact_npc(self, npc: object):
-        pass
-
+    def fight(self, monster: object, Weapon: object):
+        while monster.health > 0:
+            Weapon.use(self)
+        print(f"You killed {monster.name}.")
 class Monster:
     def __init__(self, name, health, damage):
         self.name = name
         self.health = health
         self.damage = damage
-
+    def attack(self, player: object):
+        player.health -= self.damage
 class Room:
-    def __init__(self, name, npc: bool):
+    def __init__(self, name, npc: bool, locked: bool):
         self.name = name
         self.is_npc_present = npc
+        self.locked = locked
+    def display_current(self):
+        print(self.name)
+class Key:
+    def __init__(self, name, colour, property):
+        self.name = name
+        self.colour = colour
+        self.room = property
+    def unlock_room(self,room: object):
+        if self.room == room.name:
+            room.locked = False
+            print(f"Room {room.name} has been unlocked!")
+    def unlock_door(self, door: object):
+        if self.room == door.name:
+            door.locked = False
+            print(f"Door {door.name} has been unlocked!")
 
+class NPC:
+    def __init__(self, name, room,dialogue: list, thank: list):
+        self.name = name
+        self.current_room = room
+        self.dialogue = dialogue
+        self.thank = thank
+    def interact_player(self):
+        for line in self.dialogue:
+            print(line)
+    def thank_player(self):
+        for line in self.thank:
+            print(line)
+
+class Door:
+    def __init__(self, name, locked: bool):
+        self.name = name
+        self.locked = locked
+
+    def interact(self,player: object):
+        if self.locked:
+            print(f"\n Not yet, {player.name}! You haven't opened me with the key yet!")
+            time.sleep(2)
+    
--- main.py
+++ main.py
@@ -1,120 +1,95 @@
-import pygame
-import random
+from classes import *
+import sys
+from pyfiglet import Figlet
+from colored import fg
+import time
+def scroll(text):
+    text = list(text)
+    for char in text:
+        print(char, end = "")
+        sys.stdout.flush()
+        time.sleep(0.05)
+def scroll_big(text):
+    text = list(text)
+    for char in text:
+        print(char, end = "")
+        sys.stdout.flush()
+        time.sleep(0.001)
+temp = input("Press anything to start. ")
+f = Figlet(font='the_edge')
+g = Figlet(font='big')
+print("\n")
+scroll_big(f.renderText('Treasure Game'))
+print("Hello: Please enter your name below: ")
+name = str(input("")).lower().lstrip().rstrip()
+colour = str(input("What colour do you want your player to be? (red, green, blue, black, white): ")).lower().lstrip().rstrip()
+if colour not in ['red','green','blue','black','white']:
+    print("Please input a valid colour. Defaulting to white.")
+    colour = "white"
+player = Player(name, colour, 100)
+colour = fg(colour)
+print(colour + "This is what your text will look like.")
+print("Would you like to hear your origin story?")
+choice = str(input("(y/n): ")).lower()
+if len(choice) > 1:
+    print("Please enter only yes and no. Defaulting to yes")
+elif choice == "y":
+    print("Okay, here we go!")
+    scroll("You are a pirate sailing with your crew, back to your home village in Kismaayo, Somalia, triumphant in retrieving treasures that were once a myth and were never found (no pillaging happened, I promise), when beneath the crew, there was an undiscovered leak, and you realised WAY too late.")
+    time.sleep(1)
+    scroll(" You could only watch as your ship sank to the bottom of the ocean. All is good though, as all of your crew had been safely evacuated and you still have your treasures with you- \n")
+    time.sleep(1)
 
-class Level:
-    def __init__(self, difficulty):
-        self.difficulty = difficulty
-    def set_difficulty(self, difficulty):
-        self.difficulty = difficulty
-    def new_level(self, player):
-        global walls, win_rect
-        walls = []
+    print(g.renderText("WHACK!"))
+    time.sleep(1)
+    scroll(" You feel a sharp pain in the back of your head, and everything goes black. When you opened your eyes again, you were alone, in a place that you had never seen before, with what appears to be nothing else…")
+    time.sleep(1)
+    scroll(" You wake up in some dungeon, and your story begins. You only have a vague idea of how you got here, but all you know is that you MUST escape. \n")
+    time.sleep(2)
+else:
+    print("Fine, but you WILL have little context on where you are.")
+    time.sleep(1)
 
-        if self.difficulty == "E":
-            walls_to_add = 3
-        elif self.difficulty == "M":
-            walls_to_add = 5
-        elif self.difficulty == "H":
-            walls_to_add = 10
-        for i in range(walls_to_add):
-            walls.append(self.wallgen(player))
-        win_rect = self.wallgen(player)
-        player.reset()
-
-    def wallgen(self,player):
-        while True:
-            amountx = random.randint(0,WIDTH)
-            amounty = random.randint(0,HEIGHT)
-            amountw = random.randint(50,100)
-            amounth = random.randint(50,100)
-            rect = pygame.Rect(amountx, amounty, amountw, amounth)
-            if not player.colliding_with_rect(rect):
-                return pygame.Rect(amountx, amounty, amountw, amounth)
-
-
-
-class Player(Level,object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.rect = pygame.Rect(30, 30, 60, 60)
-
-    def die(self):
-        print("You died")
-        Level.set_difficulty(self,difficulty)
-        Level.new_level(self,player)
-
-    def handle_keys(self):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT] or key[pygame.K_a]:
-            self.rect.x -= 5
-        if key[pygame.K_RIGHT] or key[pygame.K_d]:
-            self.rect.x += 5
-        if key[pygame.K_UP] or key[pygame.K_w]:
-            self.rect.y -= 5
-        if key[pygame.K_DOWN] or key[pygame.K_s]:
-            self.rect.y += 5
-        if self.rect.left < 0 or self.rect.right > WIDTH or self.rect.top < 0 or self.rect.bottom > HEIGHT:
-            self.die()
-
-    def colliding_with_rect(self, rect):
-        return self.rect.colliderect(rect)
-
-    def end_goal(self, win_rect):
-        if self.colliding_with_rect(win_rect):
-            print("You win")
-            Level.set_difficulty(self,difficulty)
-            Level.new_level(self,player)
-
-    def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0), self.rect)
-
-pygame.init()
-vec = pygame.math.Vector2 
-
-HEIGHT = 450
-WIDTH = 400
-ACC = 0.5
-FRIC = -0.12
-FPS = 120
-walls = []
-win_rect = None
-difficulty = "E"
-
-player = Player()
-
-
-
-clock = pygame.time.Clock()
-displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("game")
-
-
-print("Enter difficulty, E/M/H")
-difficulty = input().upper()
-level = Level(difficulty)
-level.new_level(player)
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    player.handle_keys()
-    for wall in walls:
-        if player.colliding_with_rect(wall):
-            player.die()
-    player.end_goal(win_rect)
-
-
-    displaysurface.fill((0,0,0))
-    for wall in walls:
-      pygame.draw.rect(displaysurface, (255, 255, 255), wall)
-    pygame.draw.rect(displaysurface, (0, 255, 0), win_rect)
-
-    player.draw(displaysurface) 
-
-    pygame.display.flip()         
-    clock.tick(60)                
+scroll_big(g.renderText("[24/11/1987, 5 pm GMT, Current Location: behind bars] \n"))
+scroll("'What the... where am I?' you think to yourself \n")
+scroll("(for the sake of the game, you can only speak in English. I know I said your village is in Somalia, but for universal purposes, you can only speak in English)\n")
+scroll(" You realise, you are by yourself, with nothing, and no one. At that moment you realise, your crew BETRAYED you. \n")
+scroll(" At that moment, you knew exactly what to do. Firstly, you break out of your jail cell, but now, you need to retrieve those items and seek REVENGE. \n")
+scroll(" While wandering past your cell, you come across a door. Naturally, you try opening it... \n")
+door1 = Door("start door", True)
+door1.interact(player)
+scroll(" Yes, the door just spoke to you. Whoever hit you must have hit you really hard. \n")
+time.sleep(2)
+scroll("'Erm, did you just speak to me?' you ask, with extreme confusion and bewilderment. But there is no response. ")
+time.sleep(1)
+scroll("'And what key?' You say as you spin around frantically. \n")
+time.sleep(3)
+scroll(" From what appears to be nowhere, a menu appears. \n")
+time.sleep(1)
+print(" Here are your options: \n 1. Search your current room \n 2. Check your inventory \n 3. Try to break the door down \n")
+time.sleep(2)
+scroll("'Wha- you know what, I'm not gonna question this anymore, I've clearly gone insane' you say. \n")
+time.sleep(1)
+scroll("Okay, so now you need to select an option. ")
+while True: 
+    choice2 = int(input("Enter option (1,2,3): "))
+    if choice2 != 1 and choice2 != 2 and choice2 != 3:
+        print("Please only input choice 1, 2 or 3.")
+        continue
+    elif choice2 == 1:
+        scroll("Desperate to escape, you start frantically searching around the room to find a red key.")
+        key1 = Key("prison door opener", "red", "start door")
+        scroll("Surely this MUST be the right key... \n")
+        scroll("You approach the door and try to open it... \n")
+        key1.unlock_door(door1)
+        scroll("Don'")
+        break
+    elif choice2 == 2:
+        scroll("You frantically open your convenient bag along your waist. From there you see: \n")
+        for item in player.inventory:
+            print(f"\n {item} \n")
+        scroll("Nothing. What did you expect? You JUST got here. Now you have to choose again.")
+    else:
+        scroll("Out of pure anger of what's just happened to you, you take a couple steps back and run into the door... \n")
+        scroll("And now you're dead. Idiot.")
+        exit()
